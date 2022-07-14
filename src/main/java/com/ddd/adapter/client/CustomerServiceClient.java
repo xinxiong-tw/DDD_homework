0c3e63f4db79fc9cfe63ec811334e84c5ac05d99
xiongxin <xin.xiong@thoughtworks.com>
2022-07-14 18:07:51 +0800

    refactor: import

--- src/main/java/com/ddd/adapter/client/CustomerServiceClient.java
+++ src/main/java/com/ddd/adapter/client/CustomerServiceClient.java
@@ -1,9 +1,10 @@
 package com.ddd.adapter.client;
 
+import com.ddd.domain.calculation.outbound.CustomerService;
 import com.ddd.domain.calculation.valueObject.CustomerInfo;
 import com.ddd.domain.calculation.valueObject.CustomerRole;
 
-public class CustomerServiceClient implements com.ddd.domain.calculation.outbound.CustomerService {
+public class CustomerServiceClient implements CustomerService {
     @Override
     public CustomerInfo getCustomerInfoById(String customerId) {
         return new CustomerInfo(customerId, new CustomerRole("role"));
--- src/main/java/com/ddd/adapter/client/ProductServiceClient.java
+++ src/main/java/com/ddd/adapter/client/ProductServiceClient.java
@@ -1,10 +1,11 @@
 package com.ddd.adapter.client;
 
+import com.ddd.domain.calculation.outbound.ProductService;
 import com.ddd.domain.calculation.valueObject.PriceTable;
 
 import java.util.List;
 
-public class ProductServiceClient implements com.ddd.domain.calculation.outbound.ProductService {
+public class ProductServiceClient implements ProductService {
     @Override
     public PriceTable getPriceTableByIds(List<String> productIds) {
         return null;
